7e61d92816d84123928c3f4b2577d5fc62ca3ead
AbrahamG <abraham.olague@gmail.com>
2019-05-31 23:43:11 -0500

    read file added, html modal fixed

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # FriendFinder
+
+### About
+
+FriendFinder is an easy to use application to input your profile and get your Best Match.
+
+### Motivation
+
+This is a project for my Tecnologico de Monterrey's Bootcamp, we are currently learning how to use Node.js along with MySQL databases.
+
+### What it does
+
+FriendFinder shows you a survey page where you input your name, a link to your picture and a list of questions. This will submit your profile to the page and also will let you know who is your best match according to an algorithm the compares the answers.
+
+###Sample
+
+![](/images/ezgif.com-video-to-gif.gif)
+
+### Authors
+
+Abraham Granillo
---
+++ app/data/friends.js
@@ -0,0 +1,55 @@
+var tableArray = [
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+
+    },
+    {
+        "name":"Sophie",
+        "photo":"https://i.ytimg.com/vi/m_S0qCeA-pc/maxresdefault.jpg",
+        "scores":[
+            5,
+            3,
+            4,
+            2,
+            4,
+            3,
+            5,
+            1,
+            2,
+            1
+        ]
+
+    },
+    {
+        "name":"Maria",
+        "photo":"https://ep01.epimg.net/elpais/imagenes/2019/01/18/gente/1547807300_947043_1547826338_noticia_normal.jpg",
+        "scores":[
+            4,
+            2,
+            3,
+            4,
+            4,
+            1,
+            4,
+            5,
+            2,
+            5
+        ]
+
+    }
+];
+
+module.exports = tableArray;
